7a09beacbbb23474e0e30f502649b26b5e0ac20e
Saranraj <www.saranrajcoder@gmail.com>
2024-04-14 22:07:47 +0530

    Home Page Added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Pages/Home'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+      <Home/>
+    </div>
   )
 }
 
