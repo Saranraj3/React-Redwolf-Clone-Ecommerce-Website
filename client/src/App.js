f020c4f338d52c30c79eb512917fc04563159105
Saranraj <www.saranrajcoder@gmail.com>
2024-04-15 16:14:57 +0530

    Some Imported Packages Installed Like Design Parts

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import Home from './Pages/Home'
+import Product from './Components/HomeCategory/Product'
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <Home/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/product/:id' element={<Product/>} />
+      </Routes>
     </div>
   )
 }
