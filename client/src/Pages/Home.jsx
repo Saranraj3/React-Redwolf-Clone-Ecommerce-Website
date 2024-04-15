39ab164c7dbba078b7eef5b441f974ab967cdbd6
Saranraj <www.saranrajcoder@gmail.com>
2024-04-15 16:14:12 +0530

    react-router-dom Routing App.js File and Navbar.js File

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -5,6 +5,7 @@ import Section from '../Components/Section/Section'
 import CategoriesSlider from '../Components/CategoriesSlider/CategoriesSlider'
 import BrandsSlider from '../Components/BrandIcons/BrandIcons'
 import HomeCategory from '../Components/HomeCategory/HomeCategory'
+import ProductList from '../Components/ProductList/ProductList'
 
 function Home() {
   return (
@@ -14,6 +15,7 @@ function Home() {
       <Section/>
       <CategoriesSlider/>
       <BrandsSlider/>
+      <ProductList/>
       <HomeCategory/>
     </div>
   )
