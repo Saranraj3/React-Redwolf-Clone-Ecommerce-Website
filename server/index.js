2f0c9ab76a00bbdd619521c93b592020e18517f5
Saranraj <www.saranrajcoder@gmail.com>
2024-04-19 12:02:00 +0530

    Utils File Created and Error Handle Code Writed

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-const {MONGODB_URL,PORT} = process.env;
+const { MONGODB_URL, PORT } = process.env;
 const app = express();
 
 app.use(express.json());
@@ -10,17 +10,27 @@ app.use(cors);
 
 
 mongoose
-  .connect(MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB is  connected successfully"))
-  .catch((err) => console.error(err));
+    .connect(MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB is  connected successfully"))
+    .catch((err) => console.error(err));
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
 
+//ERROR HANDLER
 
+app.use((err, req, res, next) => {
+    err.statucode = err.statucode || 500;
+    err.status = err.status || 'error';
+
+    res.status(err.statucode).json({
+        status: err.status,
+        message: err.message,
+    });
+});
 
 
