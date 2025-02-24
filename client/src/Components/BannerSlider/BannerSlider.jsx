d33070c5425bff447c8a3c8965d0e1b6df144505
saran.raj <saran.raj@ateamsoftsolutions.com>
2025-02-24 16:50:10 +0530

    Banner Slider Component Modifyed

--- client/src/Components/BannerSlider/BannerSlider.jsx
+++ client/src/Components/BannerSlider/BannerSlider.jsx
@@ -1,22 +1,13 @@
-import React from 'react'
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import alpha from '../../Assets/BannerSlieder/redwolf-alpha-membership.jpg'
-import dtpromo from '../../Assets/BannerSlieder/dtg-promo.jpg'
-import dropcut from '../../Assets/BannerSlieder/dropcut-t-shirts-india.jpg'
-import fullsleeve from '../../Assets/BannerSlieder/fullsleeve-t-shirts.jpg'
-import oversized from '../../Assets/BannerSlieder/oversized-t-shirts.jpg'
-import naruto from '../../Assets/BannerSlieder/Naruto.jpg'
-import tshirt from '../../Assets/BannerSlieder/t-shirts-india.jpg'
-import xmen from '../../Assets/BannerSlieder/x-men-dp-wolverine.jpg'
-import hoodies from '../../Assets/BannerSlieder/hoodies-india.jpg'
-import womentshirt from '../../Assets/BannerSlieder/t-shirts-for-women.jpg'
-import basicpremium from '../../Assets/BannerSlieder/redwolf-basics.jpg'
-
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import onepiece from "../../Assets/BannerSlieder/one-piece-color-spread-wallpapers.jpg";
+import demonSlayer from "../../Assets/BannerSlieder/Demon-slayer.jpg";
+import deathNote from "../../Assets/BannerSlieder/Death-Note.jpg";
+import attackOnTitan from "../../Assets/BannerSlieder/Attack-On-Titan.jpg";
 
 function BannerSlider() {
-
   const settings = {
     dots: false,
     speed: 500,
@@ -29,44 +20,23 @@ function BannerSlider() {
   };
 
   return (
-    <div className=' overflow-x-hidden'>
-      <Slider {...settings} className='overflow-x-hidden'>
+    <div className=" overflow-x-hidden">
+      <Slider {...settings} className="overflow-x-hidden">
         <div>
-          <img src={alpha} alt="" />
+          <img src={onepiece} alt="" />
         </div>
         <div>
-          <img src={dtpromo} alt="" />
+          <img src={demonSlayer} alt="" />
         </div>
         <div>
-          <img src={dropcut} alt="" />
+          <img src={deathNote} alt="" />
         </div>
         <div>
-          <img src={fullsleeve} alt="" />
-        </div>
-        <div>
-          <img src={oversized} alt="" />
-        </div>
-        <div>
-          <img src={naruto} alt="" />
-        </div>
-        <div>
-          <img src={tshirt} alt="" />
-        </div>
-        <div>
-          <img src={xmen} alt="" />
-        </div>
-        <div>
-          <img src={hoodies} alt="" />
-        </div>
-        <div>
-          <img src={womentshirt} alt="" />
-        </div>
-        <div>
-          <img src={basicpremium} alt="" />
+          <img src={attackOnTitan} alt="" />
         </div>
       </Slider>
     </div>
-  )
+  );
 }
 
-export default BannerSlider
+export default BannerSlider;
