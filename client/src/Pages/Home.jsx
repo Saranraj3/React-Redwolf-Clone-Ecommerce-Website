ede17df93a0a6a195bb25e4fa68662ad8d5f115c
saran.raj <saran.raj@ateamsoftsolutions.com>
2025-02-25 23:59:33 +0530

    Components Import Comment Removed

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import BannerSlider from '../Components/BannerSlider/BannerSlider'
 import Section from '../Components/Section/Section'
-import CategoriesSlider from '../Components/CategoriesSlider/CategoriesSlider'
-import BrandsSlider from '../Components/BrandIcons/BrandIcons'
 import HomeCategory from '../Components/HomeCategory/HomeCategory'
 import ProductList from '../Components/ProductList/ProductList'
-import PressLogos from '../Components/PressLogos/PressLogos'
 import Description from '../Components/Description/Description'
 
 function Home() {
