9b1773c5bbe07dc74d28fa7b0a9ad0ff428ae2d8
saran.raj <saran.raj@ateamsoftsolutions.com>
2025-03-02 17:28:34 +0530

    Background Color Added

--- client/src/Components/Description/Description.jsx
+++ client/src/Components/Description/Description.jsx
@@ -1,18 +1,44 @@
-import React from 'react'
+import React from "react";
 
 function Description() {
-    return (
-        <div className='sm:m-4 sm:p-3 sm:max-w-[100%] box-border border rounded-md bg-gray-100 '>
-            <h1 className='text-2xl font-extrabold text-center text-gray-700  mt-5' >MANGA, COMIC, MANHWA, GRAPHIC NOVELS, LIGHT NOVELS & ONLINE SHOP FOR YOU</h1>
-            <p className=' mt-5 text-sm text-gray-500'>Redwolf is an indie clothing label that brings you the most amazing t-shirts and accessories online inspired by everything pop culture! Redwolf offers a wide range of products from cool t shirts and sweatshirts to accessories like badges, posters, laptop skins and fridge magnets. A t-shirt is a perfect canvas to showcase your creativity, passion and personality. At Redwolf, we work with some of the best designers and illustrators from all around the globe to offer you a fresh and unique selection of designer</p>
-            <p className='text-sm text-red-500'>t-shirts</p>
-            <p className=' text-sm mt-3 text-gray-500'>If you're looking for a t-shirt to help you express yourself and the things you love, we have you covered. Check out our collection of cool t shirts online in India. From </p>
-            <p className=' text-sm text-red-500'>funny t-shirts</p>
-            <p className='text-sm mt-3 text-gray-500 '>with epic parodies and hilarious one liners to intricate graphic designs inspired by your favourite movie and TV show characters. We have something for everyone! Our collection of online t shirts are screen printed to perfection on 100% cotton Redwolf tshirts. We provide cash on delivery and free shipping all over India with a hassle free return policy to make sure you have a pleasant online shopping experience.</p>
-            <p className='text-sm text-gray-500 '>All artwork posted on this website is intended as fan art and is not purported to be official merchandise unless indicated otherwise. If you have any issues regarding the artwork, do write in to us at </p>
-            <p className='text-sm text-red-500'>contact@redwolf.in</p>
-        </div>
-    )
+  return (
+    <div className="sm:m-4 sm:p-3 sm:max-w-[100%] box-border border rounded-md bg-LightBlack text-white ">
+      <h1 className="text-2xl font-extrabold text-center mt-5">
+        MANGA, COMIC, MANHWA, GRAPHIC NOVELS, LIGHT NOVELS & ONLINE SHOP FOR YOU
+      </h1>
+      <p className=" mt-5 text-sm">
+        Redwolf is an indie clothing label that brings you the most amazing
+        t-shirts and accessories online inspired by everything pop culture!
+        Redwolf offers a wide range of products from cool t shirts and
+        sweatshirts to accessories like badges, posters, laptop skins and fridge
+        magnets. A t-shirt is a perfect canvas to showcase your creativity,
+        passion and personality. At Redwolf, we work with some of the best
+        designers and illustrators from all around the globe to offer you a
+        fresh and unique selection of designer
+      </p>
+      <p className="text-sm text-red-500">t-shirts</p>
+      <p className=" text-sm mt-3">
+        If you're looking for a t-shirt to help you express yourself and the
+        things you love, we have you covered. Check out our collection of cool t
+        shirts online in India. From{" "}
+      </p>
+      <p className=" text-sm text-red-500">funny t-shirts</p>
+      <p className="text-sm mt-3">
+        with epic parodies and hilarious one liners to intricate graphic designs
+        inspired by your favourite movie and TV show characters. We have
+        something for everyone! Our collection of online t shirts are screen
+        printed to perfection on 100% cotton Redwolf tshirts. We provide cash on
+        delivery and free shipping all over India with a hassle free return
+        policy to make sure you have a pleasant online shopping experience.
+      </p>
+      <p className="text-sm">
+        All artwork posted on this website is intended as fan art and is not
+        purported to be official merchandise unless indicated otherwise. If you
+        have any issues regarding the artwork, do write in to us at{" "}
+      </p>
+      <p className="text-sm text-red-500">contact@redwolf.in</p>
+    </div>
+  );
 }
 
-export default Description
+export default Description;
